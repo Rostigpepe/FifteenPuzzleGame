325316d92d701975e114e0f043afc48511b03dba
Robin Stenskytt <robin.olivemark@gmail.com>
2021-10-22 12:54:47 +0200

    Adding GUITesting class, and lil funky code

---
+++ src/fifteenpuzzle/GUITesting.java
@@ -0,0 +1,41 @@
+package fifteenpuzzle;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUITesting implements ActionListener {
+
+    private int count = 0;
+    private JFrame jf;
+    private JLabel jl;
+    private JPanel jp;
+
+
+    GUITesting(){
+        jf = new JFrame();
+        JButton button = new JButton("Test button");
+        button.addActionListener(this);
+
+        jl = new JLabel("Clicks: 0");
+        jp = new JPanel();
+
+        jp.setBorder(BorderFactory.createEmptyBorder(100, 100, 50, 100));
+        jp.setLayout(new GridLayout(4, 4));
+        jp.add(button);
+        jp.add(jl);
+
+        jf.add(jp, BorderLayout.CENTER);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setTitle("Test GUI");
+        jf.pack();
+        jf.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        jl.setText("Clicks: " + count);
+    }
+}
