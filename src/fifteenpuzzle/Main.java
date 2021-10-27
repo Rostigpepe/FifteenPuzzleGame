dadb892088b708c9ef4c9453f271c2289ea3be18
Robin Stenskytt <robin.olivemark@gmail.com>
2021-10-28 00:27:21 +0200

    Merged Win_Condition into GUIDevelopment

--- src/fifteenpuzzle/Main.java
+++ src/fifteenpuzzle/Main.java
@@ -4,18 +4,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        boolean open = true;
-        String size = JOptionPane.showInputDialog("Choose size of the game");
-
-        while(open){
-            try{
-                int gameSize = Integer.parseInt(size);
-                new GameAndFunctions(gameSize);
-                open = false;
-            }
-            catch(NumberFormatException ignored){
-                size = JOptionPane.showInputDialog("Only enter a number");
-            }
-        }
+        GameAndFunctions.startUpOptions();
     }
 }
