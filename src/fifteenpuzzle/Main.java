8ec2cfc454a616303de247307de90036a4f6cd33
Robin Stenskytt <robin.olivemark@gmail.com>
2021-10-27 23:51:06 +0200

    Added class for the options Added methods to implement it Made by Fredrik

--- src/fifteenpuzzle/Main.java
+++ src/fifteenpuzzle/Main.java
@@ -1,9 +1,21 @@
 package fifteenpuzzle;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        boolean open = true;
+        String size = JOptionPane.showInputDialog("Choose size of the game");
 
-        GameAndFunctions mgw = new GameAndFunctions(4);
-
+        while(open){
+            try{
+                int gameSize = Integer.parseInt(size);
+                new GameAndFunctions(gameSize);
+                open = false;
+            }
+            catch(NumberFormatException ignored){
+                size = JOptionPane.showInputDialog("Only enter a number");
+            }
+        }
     }
 }
